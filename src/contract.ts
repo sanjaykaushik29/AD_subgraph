ef8dfa0f5e0f8742f46aad9e9d992605d57c28e0
sanjaykaushik29 <thoughtwin.sanjay@gmail.com>
2023-07-21 11:50:38 +0530

    AD_subgraph

--- src/contract.ts
+++ src/contract.ts
@@ -1,46 +1,20 @@
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  Upgraded as UpgradedEvent
+  CreateLaunchpad as AdminChangedEvent,
 } from "../generated/Contract/Contract"
-import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
+import { CreateLaunchpad, } from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
+export function handleAdminCreateLaunchpad(event: AdminChangedEvent): void {
+  let entity = new CreateLaunchpad(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  entity.creator = event.params.creator
+  entity.collection = event.params.collection
 
+  entity.name = event.params.name
+  entity.symbol = event.params.symbol
+  entity.contractURI = event.params.contractURI
+  entity.maxSupply = event.params.maxSupply
   entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
   entity.save()
 }
